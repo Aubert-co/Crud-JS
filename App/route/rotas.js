d51403d5f4c0f3783b7c69121aed796abdadc8bf
Black <53127242+Aubert-co@users.noreply.github.com>
2021-06-22 23:23:25 -0300

    Create rotas.js

---
+++ App/route/rotas.js
@@ -0,0 +1,12 @@
+const route = require('express').Router()
+const cors = require('cors')
+const index = require('./index')
+const bodyParser = require('body-parser')
+
+
+route.use(cors())
+route.use(bodyParser.json())
+route.use(index)
+
+
+module.exports = route
