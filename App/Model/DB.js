a756555f2c4e5b3b223a38e6e2f8672681eca460
Black <53127242+Aubert-co@users.noreply.github.com>
2021-06-28 23:23:15 -0300

    Update DB.js

---
+++ App/Model/DB.js
@@ -0,0 +1,16 @@
+const mongo = require('mongodb')
+
+require('dotenv').config()
+
+mongo.connect(`${process.env.HOST_MONGO}`,{useUnifiedTopology:true,useNewUrlParser:true})
+
+const Schema = mongo.Schema
+
+const schema = new Schema({
+    price_item:String,
+    name_item:String
+})
+
+const Itens = mongo.model('itens',schema)
+
+module.exports = Itens
