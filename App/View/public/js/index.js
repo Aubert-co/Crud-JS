4a36ca19d3b723b08f9aab5ce4c1124bbd35533c
Black <53127242+Aubert-co@users.noreply.github.com>
2021-06-30 23:30:24 -0300

    Update index.js

--- App/View/public/js/index.js
+++ App/View/public/js/index.js
@@ -1,6 +1,16 @@
-const DivMain = document.querySelector('data-div')
+const DivContent = document.querySelector('#content')
 
 
-const template = `
+/*const template = `
+<table>
+<tr>
+    <th>item1</th>
+    <th>item2</th>
+</tr>
 
-`
+<tr>
+    <td>${item}</td>
+    <td>${itens}</td>
+</tr>
+</table>
+`*/
