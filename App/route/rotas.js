e0d716c4c0c47ee89b2386d879a8a33a85d9705a
Black <53127242+Aubert-co@users.noreply.github.com>
2021-07-03 23:41:33 -0300

    Update rotas.js

--- App/route/rotas.js
+++ App/route/rotas.js
@@ -1,12 +1,14 @@
 const route = require('express').Router()
 const cors = require('cors')
+
 const index = require('./index')
 const bodyParser = require('body-parser')
 
 
 route.use(cors())
 route.use(bodyParser.json())
-route.use(index)
+
+route.use('/api',index)
 
 
 module.exports = route
