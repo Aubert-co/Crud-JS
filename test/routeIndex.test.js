716c25a1aeb0b699830a6b430233a15dcd9f903d
Black <53127242+Aubert-co@users.noreply.github.com>
2021-07-05 22:53:35 -0300

    Update routeIndex.test.js

--- test/routeIndex.test.js
+++ test/routeIndex.test.js
@@ -1,4 +1,5 @@
 const chai = require('chai')
+const {expect} = require('chai')
 const should = chai.should()
 const chaiHTTP = require('chai-http')
 const app = require('../App/serve')
@@ -16,6 +17,22 @@ describe('ITENS ',()=>{
             if(err)done(err)
 
             res.should.have.status(200)
+            expect(res.body).to.deep.own.include({msg:'sucessful'})
+            done()
+        })
+    })
+
+    it('should return a status 200 and a sucessl menssage',(done)=>{
+        chai.request(app)
+        .post('/api/itens')
+        .set({'Content-Type':'application/json'})
+        .send({name_item:'camisa',price_item:10})
+        .end((err,res)=>{
+            if(err)done(err)
+
+            res.should.have.status(200)
+
+            expect(res.body).to.deep.own.include({msg:'post sucessful'})
             done()
         })
     })
