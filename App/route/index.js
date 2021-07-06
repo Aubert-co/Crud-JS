e4336aed62867679c7acfe20e1415496b6bcd5f7
Black <53127242+Aubert-co@users.noreply.github.com>
2021-07-05 22:53:30 -0300

    Update index.js

--- App/route/index.js
+++ App/route/index.js
@@ -1,7 +1,20 @@
 const route = require('express').Router()
 
+
+
+
 route.get('/itens',(req,res)=>{
-    res.status(200).send({data:'ola'})
+    res.status(200).send({msg:'sucessful'})
+})
+
+.post('/itens',(req,res)=>{
+
+    const {name_item,price_item} = req.body
+
+    if(!name_item || !price_item){
+        return res.status(404).send({msg:'erro'})
+    }
+    res.status(200).send({msg:'post sucessful'})
 })
 
 module.exports = route
