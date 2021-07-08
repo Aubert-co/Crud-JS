5f5f406872694f39f2dc6fc46634ea4731abb15b
Black <53127242+Aubert-co@users.noreply.github.com>
2021-07-07 23:31:04 -0300

    Update index.js

--- App/route/index.js
+++ App/route/index.js
@@ -5,18 +5,19 @@ const route = require('express').Router()
 
 route
 .get('/itens',(req,res)=>{
-    const data =['name']
+    const data =[]
 
 
     res.status(200).send(data)
 })
 
+
 .post('/itens',(req,res)=>{
 
     const {name_item,price_item} = req.body
 
-    if(!name_item || !price_item){
-        return res.status(404).send({msg:'erro'})
+    if(typeof name_item !== 'string' || typeof price_item !=='number' || name_item ===''){
+        return res.status(404).send({msg:'wrong datas'})
     }
     res.status(200).send({msg:'post sucessful'})
 })
@@ -26,10 +27,19 @@ route
     const {id} = req.body
 
 
-    if(typeof id !== 'number'  &&  typeof id !== 'object')return res.status(404).send({msg:'erro ao deletar'})
-
+    if(typeof id !== 'number'  &&  typeof id !== 'object'){
+        return res.status(404).send({msg:'not found'})
+    }
     res.status(200).send({msg:'delete sucessful'})
 })
 
+.put('/UpdteItens',(req,res)=>{
+    const {name_item,price_item,id} = req.body
+
+    if(typeof name_item !== 'string' || typeof price_item !== 'number' || typeof id !=='number' || name_item ===''){
+        return res.status(404).send({msg:'not found'})
+    }
+    res.status(200).send({msg:'update sucessful'})
+})
 
 module.exports = route
