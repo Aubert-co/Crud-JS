97eae6031af87b49fd3dd1c9e8783d1b1cabc78c
Black <53127242+Aubert-co@users.noreply.github.com>
2021-07-16 22:54:55 -0300

    Update index.js

--- App/route/index.js
+++ App/route/index.js
@@ -1,40 +1,58 @@
 const route = require('express').Router()
-
+const ItensDB = require('../Model/DB')
 
 route
-.get('/itens',(req,res)=>{
-    const data =[]
-
-    res.status(200).send(data)
+.get('/itens',async(req,res)=>{
+  
+    res.status(200).send('data')
 })
 
 
-.post('/itens',(req,res)=>{
+.post('/itens',async (req,res)=>{
 
-    const {name_item,price_item} = req.body
+    const {name_item,price_item,color_item,quantities_item,ItensID} = req.body
 
     if(typeof name_item !== 'string' || typeof price_item !=='number' || name_item ===''){
         return res.status(404).send({msg:'wrong datas'})
     }
+    try{
+        ItensDB.insertMany({
+            name_item:'camisa',
+            price_item, 
+            color_item,
+            quantities_item
+        })
+    }catch(err){
+        throw err
+    }
     res.status(200).send({msg:'post sucessful'})
 })
 
-.delete('/deleteItens',(req,res)=>{
+.delete('/itens',(req,res)=>{
 
-    const {id} = req.body
+    const {id_itens} = req.body
 
 
-    if(typeof id !== 'number'  &&  typeof id !== 'object'){
+    if(typeof id_itens !== 'number'  &&  typeof id_itens !== 'object'){
         return res.status(404).send({msg:'not found'})
     }
     res.status(200).send({msg:'delete sucessful'})
 })
 
-.put('/UpdteItens',(req,res)=>{
-    const {name_item,price_item,id} = req.body
+.put('/itens',(req,res)=>{
+    const {id_itens,name_item,price_item,color_item,quantities_item} = req.body
 
-    if(typeof name_item !== 'string' || typeof price_item !== 'number' || typeof id !=='number' || name_item ===''){
+    if(typeof name_item !== 'string' || typeof price_item !== 'number' ||  id_itens ==='' || name_item ===''){
         return res.status(404).send({msg:'not found'})
+    }
+    try{
+    ItensDB.updateOne(
+        {id_itens:id_itens},
+        {
+            $set:{name_item,price_item,color_item,quantities_item}
+        })
+    }catch(err){
+        if(err)throw err
     }
     res.status(200).send({msg:'update sucessful'})
 })
