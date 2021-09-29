736e925f6a6929f7de4a756a2663cb53709b54bf
Black <53127242+Aubert-co@users.noreply.github.com>
2021-09-29 16:26:33 -0300

    Create index.js

---
+++ App/Controller/index.js
@@ -0,0 +1,54 @@
+const ItensDB = require('../Model/DB')
+const Routes = {
+    GetItens:async(req,res)=>{
+        const data = await ItensDB.find({})
+        res.status(200).send(data)
+    },
+    PostItens:async(req,res)=>{
+        const {name_item,price_item,color_item,quantities_item,ItensID} = req.body
+
+    if(typeof name_item !== 'string' || typeof price_item !=='number' || name_item ===''
+     || color_item === '' || quantities_item === ''){
+        return res.status(404).send({msg:'wrong datas'})
+    }
+    try{
+        ItensDB.insertMany({
+            name_item:'camisa',
+            price_item, 
+            color_item,
+            quantities_item
+        })
+    }catch(err){
+        throw err
+    }
+    res.status(200).send({msg:'post sucessful'})
+    },
+    DeleteItens:async(req,res)=>{
+        const {id_itens} = req.body
+
+
+    if(typeof id_itens !== 'number'  &&  typeof id_itens !== 'object'){
+        return res.status(404).send({msg:'not found'})
+    }
+    res.status(200).send({msg:'delete sucessful'})
+    },
+    UpdateItens:async(req,res)=>{
+        const {id_itens,name_item,price_item,color_item,quantities_item} = req.body
+
+        if(typeof name_item !== 'string' || typeof price_item !== 'number' ||  id_itens ==='' || name_item ===''){
+            return res.status(404).send({msg:'not found'})
+        }
+        try{
+        ItensDB.updateOne(
+            {id_itens:id_itens},
+            {
+                $set:{name_item,price_item,color_item,quantities_item}
+            })
+        }catch(err){
+            if(err)throw err
+        }
+        res.status(200).send({msg:'update sucessful'})
+    }
+}
+
+module.exports = Routes
