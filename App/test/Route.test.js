6c5fac4804eff8c3fa74b70e5a7e24233139de5e
Black <53127242+Aubert-co@users.noreply.github.com>
2021-09-30 23:21:33 -0300

    Create Route.test.js

---
+++ App/test/Route.test.js
@@ -0,0 +1,14 @@
+const chai = require('chai')
+const {expect} = require('chai')
+const request = require('supertest')
+const app = require('../serve')
+
+
+describe('APIS Itens ',()=>{
+    test('should return a status 200 and a data array get/itens',async()=>{
+       const resp = await request(app)
+       .get('/api/itens')
+
+       expect(resp.statusCode).to.equal(200)
+    })
+})
