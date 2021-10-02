4fb69785befe0c7bdb8296bf80a6531ba8a62c9c
Black <53127242+Aubert-co@users.noreply.github.com>
2021-10-01 22:43:05 -0300

    Update Route.test.js

--- App/test/Route.test.js
+++ App/test/Route.test.js
@@ -8,7 +8,10 @@ describe('APIS Itens ',()=>{
     test('should return a status 200 and a data array get/itens',async()=>{
        const resp = await request(app)
        .get('/api/itens')
+        
 
-       expect(resp.statusCode).to.equal(200)
+        expect(resp.statusCode).to.equal(200)
+        expect(resp.body.data).to.be.a('array')
+    
     })
 })
