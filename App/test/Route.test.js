2a7d9485e13c04c18fe880598d8334260e2f75d6
Black <53127242+Aubert-co@users.noreply.github.com>
2021-10-02 23:42:18 -0300

    Update Route.test.js

--- App/test/Route.test.js
+++ App/test/Route.test.js
@@ -15,10 +15,18 @@ describe('APIS Itens ',()=>{
     
     })
 
-    test('should return a status 404 when send invalid datas  post/itens',async()=>{
+    test('should return a status 404 when send invalid name_item  post/itens',async()=>{
         const resp = await request(app)
         .post('/api/itens')
         .send({name_item:32,price_item:43,color_item:'blue',quantities_item:34})
+
+        expect(resp.statusCode).to.equal(404)
+        expect(resp.body.msg).to.equal('invalid datas')
+    })
+    test('should return a status 404 when send invalid price_item  post/itens',async()=>{
+        const resp = await request(app)
+        .post('/api/itens')
+        .send({name_item:'camisa',price_item:'43',color_item:'blue',quantities_item:34})
 
         expect(resp.statusCode).to.equal(404)
         expect(resp.body.msg).to.equal('invalid datas')
